6524b6b79c4fe92c8616ab9d2036e303ea6bc303
bjtj10 <bjtj10@gmail.com>
2015-07-26 14:17:58 +0900

    initial commit

---
+++ build.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+BASE=$PWD
+DIR_BUILD=$PWD/build
+DIR_WORLD=$PWD/world
+
+OPT=build
+
+if [ -n "$1" ]; then
+	OPT=$1
+fi
+
+case $OPT in
+	build)
+		autoreconf -i
+		
+		mkdir -p $DIR_BUILD
+		mkdir -p $DIR_WORLD
+		
+		cd $DIR_BUILD
+		
+		$BASE/configure --prefix "$DIR_WORLD" && make && make install
+		;;
+	make)
+		cd $DIR_BUILD && make && make install
+		;;
+	dist)
+		cd $DIR_BUILD && make dist
+		;;
+	clean)
+		rm -rf $DIR_BUILD $DIR_WORLD
+		;;
+	*)
+		echo "unknown option - $OPT"
+		exit 1
+		;;
+esac
