63870164c4291c6d9007699792729c167590ee1a
bjtj <bjtj10@gmail.com>
2015-10-21 22:27:21 +0900

    xcode oslayer sample project added

---
+++ xcode/oslayer sample/oslayer sample/main.m
@@ -0,0 +1,19 @@
+//
+//  main.m
+//  oslayer sample
+//
+//  Created by TJ on 2015. 10. 21..
+//  Copyright © 2015년 TJ. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "../../oslayer/oslayer/oslayer.h"
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"OS version : %@", [oslayer nomeaningfulVesion]);
+    }
+    return 0;
+}
