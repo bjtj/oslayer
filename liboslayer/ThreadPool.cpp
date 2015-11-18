bc8d4893e44b5b3660739c4c6b2c97d80be14cdb
bjtj <bjtj10@gmail.com>
2015-11-18 17:25:48 +0900

    ThreadPool added

---
+++ liboslayer/ThreadPool.cpp
@@ -0,0 +1,110 @@
+#include "ThreadPool.hpp"
+
+namespace UTIL {
+
+	using namespace std;
+	using namespace OS;
+
+	
+	ThreadPool::ThreadPool(size_t poolSize, InstanceCreator<FlaggableThread*> & creator) : freeQueueLock(1), workingQueueLock(1), poolSize(poolSize), creator(creator), running(false) {
+
+		for (size_t i = 0; i < poolSize; i++) {
+			freeQueue.push_back(creator.createInstance());
+		}
+	}
+
+	
+	ThreadPool::~ThreadPool() {
+
+		stop();
+
+		collectUnflaggedThreads();
+
+		for (deque<FlaggableThread*>::const_iterator iter = freeQueue.begin(); iter != freeQueue.end(); iter++) {
+			FlaggableThread * thread = *iter;
+
+			thread->interrupt();
+			thread->join();
+
+			creator.releaseInstance(thread);
+		}
+	}
+
+	void ThreadPool::start() {
+
+		if (!running) {
+
+			for (deque<FlaggableThread*>::const_iterator iter = freeQueue.begin(); iter != freeQueue.end(); iter++) {
+				FlaggableThread * thread = *iter;
+				thread->start();
+			}
+
+			running = true;
+		}
+
+	}
+	void ThreadPool::stop() {
+
+		if (running) {
+
+			workingQueueLock.wait();
+			for (deque<FlaggableThread*>::const_iterator iter = workingQueue.begin(); iter != workingQueue.end(); iter++) {
+				FlaggableThread * thread = *iter;
+				thread->setFlag(false);
+			}
+			workingQueueLock.post();
+
+			running = false;
+		}
+	}
+
+	void ThreadPool::collectUnflaggedThreads() {
+
+		workingQueueLock.wait();
+		for (deque<FlaggableThread*>::iterator iter = workingQueue.begin(); iter != workingQueue.end();) {
+			FlaggableThread * thread = *iter;
+			if (!thread->flagged()) {
+				release(thread);
+				iter = workingQueue.erase(iter);
+			} else {
+				iter++;
+			}
+		}
+		workingQueueLock.post();
+
+	}
+
+	FlaggableThread * ThreadPool::acquire() {
+
+		freeQueueLock.wait();
+		FlaggableThread * thread = freeQueue.size() > 0 ? freeQueue.front() : NULL;
+		if (thread) {
+			thread->setFlag(false);
+			freeQueue.pop_front();
+		}
+		freeQueueLock.post();
+
+		return thread;
+	}
+	void ThreadPool::release(FlaggableThread * thread) {
+		freeQueueLock.wait();
+		freeQueue.push_back(thread);
+		freeQueueLock.post();
+	}
+	void ThreadPool::enqueue(FlaggableThread * thread) {
+		workingQueueLock.wait();
+		workingQueue.push_back(thread);
+		thread->setFlag(true);
+		workingQueueLock.post();
+	}
+	FlaggableThread * ThreadPool::dequeue() {
+		workingQueueLock.wait();
+		FlaggableThread * thread = workingQueue.size() > 0 ? workingQueue.front() : NULL;
+		if (thread) {
+			workingQueue.pop_front();
+		}
+		workingQueueLock.post();
+		return thread;
+	}
+
+}
---
+++ liboslayer/ThreadPool.hpp
@@ -0,0 +1,72 @@
+#ifndef __POOL_HPP__
+#define __POOL_HPP__
+
+#include "os.hpp"
+#include <deque>
+
+namespace UTIL {
+
+	class PoolThread;
+
+	/**
+	 * @brief ThreadMaker
+	 */
+	template<typename T>
+	class InstanceCreator {
+	private:
+	public:
+		InstanceCreator() {}
+		virtual ~InstanceCreator() {}
+
+		virtual T createInstance() = 0;
+		virtual void releaseInstance(T inst) = 0;
+	};
+
+	/**
+	 * @brief PoolThread
+	 */
+	class FlaggableThread : public OS::Thread {
+	private:
+		bool flag;
+	public:
+		FlaggableThread(bool initialFlag) : flag(initialFlag) {}
+		virtual ~FlaggableThread() {}
+
+		bool flagged() {return flag;}
+		void setFlag(bool flag) {this->flag = flag;}
+	};
+
+	/**
+	 *
+	 */
+	class ThreadPool {
+	private:
+
+		OS::Semaphore freeQueueLock;
+		std::deque<FlaggableThread*> freeQueue;
+
+		OS::Semaphore workingQueueLock;
+		std::deque<FlaggableThread*> workingQueue;
+
+		size_t poolSize;
+		InstanceCreator<FlaggableThread*> & creator;
+
+		bool running;
+
+	public:
+		ThreadPool(size_t poolSize, InstanceCreator<FlaggableThread*> & creator);
+		virtual ~ThreadPool();
+
+		void start();
+		void stop();
+		void collectUnflaggedThreads();
+
+		FlaggableThread * acquire();
+		void release(FlaggableThread * thread);
+		void enqueue(FlaggableThread * thread);
+		FlaggableThread * dequeue();
+	};
+
+}
+
+#endif
