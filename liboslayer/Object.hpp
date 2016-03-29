c7d212381620e7094512f800dfc359f1a8e49faf
bjtj <bjtj10@gmail.com>
2016-03-29 10:18:20 +0900

    bare object class

---
+++ liboslayer/Object.hpp
@@ -0,0 +1,17 @@
+#ifndef __OBJECT_HPP__
+#define __OBJECT_HPP__
+
+namespace UTIL {
+
+	/**
+	 * @brief placeholder
+	 */
+	class Object {
+	private:
+	public:
+		Object() {}
+		virtual ~Object() {}
+	};
+}
+
+#endif
