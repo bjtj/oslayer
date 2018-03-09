b6a48a2b9b89b53fad0b83017424af07c9782e26
bjtj <bjtj10@gmail.com>
2018-03-09 14:38:16 +0900

    sample pseudo code for redesigning ArgumentParser and Logger APIs

---
+++ examples/logger.cpp
@@ -0,0 +1,24 @@
+#include <liboslayer/File.hpp>
+#include <liboslayer/Logger.hpp>
+
+using namespace OS;
+using namespace logger;
+
+void foo(AutoRef<Logger> logger) {
+	logger->logd("hello in foo");
+}
+
+int main(int argc, char *argv[])
+{
+	logd("hello");
+
+	setLogOutput(File("log.out", "wb"));
+	setLogLevel(Level::DEBUG);
+	setLogFormat("[$date] $msg")
+	
+	logd("hello world");
+
+	foo(AutoRef<Logger>(new Logger("foo.log")));
+	
+    return 0;
+}
