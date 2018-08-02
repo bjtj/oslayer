f365934583e80af0df5fe38a36b5ac13b147faf2
bjtj <bjtj10@gmail.com>
2018-08-02 11:49:00 +0900

    tokenizer added

---
+++ liboslayer/Tokenizer.hpp
@@ -0,0 +1,31 @@
+#ifndef __TOKENIZER_HPP__
+#define __TOKENIZER_HPP__
+
+#include <string>
+#include <vector>
+
+namespace osl {
+	class Tokenizer
+	{
+	private:
+		std::string _text;
+		std::string _delim;
+		std::vector<std::string> _tokens;
+	public:
+		Tokenizer(const std::string & text, const std::string & delim);
+		virtual ~Tokenizer();
+		virtual void tokenize();
+		std::string & text();
+		std::string text() const;
+		std::string & delim();
+		std::string delim() const;
+		std::vector<std::string> & tokens();
+		std::vector<std::string> tokens() const;
+		std::string & token(size_t idx);
+		std::string token(size_t idx) const;
+		size_t size() const;
+	};
+
+}
+
+#endif
